a5e70a04d2c85a2d415e6df10fb10c3dc90fce31
Jarod Long <jlong22@me.com>
2015-02-15 12:03:19 -0800

    Added touch input.

--- Assets/Scripts/GGInputComponent.cs
+++ Assets/Scripts/GGInputComponent.cs
@@ -29,7 +29,39 @@ public class GGInputComponent: MonoBehaviour {
 	}
 	
 	private void UpdateTouchInput() {
-		// TODO
+		if (Input.touchCount > 0) {
+			var touch = Input.GetTouch(0);
+			
+			if (touch.phase == TouchPhase.Ended) {
+				if (this.inputOrigin.HasValue && this.currentInput.HasValue) {
+					this.Shoot();
+				}
+				
+				this.inputOrigin  = null;
+				this.currentInput = null;
+			}
+			else {
+				this.currentInput = this.ConvertInputToWorldSpace(touch.position);
+				
+				if (!this.inputOrigin.HasValue) {
+					this.inputOrigin = this.currentInput;
+				}
+				
+				var inputVector = this.inputOrigin.Value - this.currentInput.Value;
+				var magnitude   = inputVector.magnitude;
+				
+				if (magnitude < GGInputComponent.minInputThreshold) {
+					this.currentInput = null;
+				}
+				else if (magnitude > GGInputComponent.maxInputThreshold) {
+					this.currentInput = this.inputOrigin.Value - inputVector.normalized * GGInputComponent.maxInputThreshold;
+				}
+			}
+		}
+		else {
+			this.inputOrigin  = null;
+			this.currentInput = null;
+		}
 	}
 	
 	private void UpdateMouseInput() {
