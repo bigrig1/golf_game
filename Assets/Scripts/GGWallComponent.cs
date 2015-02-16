85a3de012a405bb766cb8108605aad69d068b054
Jarod Long <jlong22@me.com>
2015-02-16 12:55:34 -0800

    First pass at implementing walls.

---
+++ Assets/Scripts/GGWallComponent.cs
@@ -0,0 +1,14 @@
+//
+// The component that manages a section of wall.
+//
+
+using System.Collections;
+using UnityEngine;
+
+public class GGWallComponent: MonoBehaviour {
+	/* Initializing. */
+	
+	public void Start() {
+		// Nothing yet.
+	}
+}
