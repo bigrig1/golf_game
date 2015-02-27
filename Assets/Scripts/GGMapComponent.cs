624d529b48e8f59a0082fb3b434146c8ae5e8215
Jarod Long <jlong22@me.com>
2015-02-27 13:48:29 -0800

    Initial work on generating platforms.

--- Assets/Scripts/GGMapComponent.cs
+++ Assets/Scripts/GGMapComponent.cs
@@ -54,8 +54,11 @@ public class GGMapComponent: MonoBehaviour {
 	// Procedurally generates a map by creating all the platforms and walls for it with the given Y
 	// offset. You'll always get the same map for a given map index.
 	public void BuildMap(int mapIndex, float yOffset) {
-		var mapHeight = GGMapComponent.mapHeight;
-		var wallY     = 0.0f;
+		var mapWidth             = GGMapComponent.mapWidth;
+		var mapHeight            = GGMapComponent.mapHeight;
+		var horizontalMapPadding = GGMapComponent.horizontalMapPadding;
+		var usableMapWidth       = mapWidth - horizontalMapPadding * 2.0f;
+		var wallY                = 0.0f;
 		
 		while (wallY < mapHeight) {
 			wallY += this.AddWall(wallY + yOffset, true);
@@ -67,6 +70,50 @@ public class GGMapComponent: MonoBehaviour {
 			wallY += this.AddWall(wallY + yOffset, false);
 		}
 		
+		// TODO: This should be random between ~4-6, probably skewed so that the higher the map
+		// index, the more likely the number will be lower.
+		var sectionCount      = 5;
+		var divisionCount     = sectionCount - 1;
+		var divisionPoints    = new float[divisionCount];
+		var baseSectionHeight = mapHeight / (float)sectionCount;
+		
+		for (var i = 0; i < divisionCount; i += 1) {
+			// TODO: Plus or minus some random offset for variety.
+			divisionPoints[i] = (float)(i + 1) * baseSectionHeight;
+		}
+		
+		var sectionY = yOffset;
+		
+		for (var i = 0; i < sectionCount; i += 1) {
+			var sectionHeight      = i == sectionCount - 1 ? mapHeight - sectionY : divisionPoints[i] - sectionY;
+			var innerSectionHeight = sectionHeight * GGMapComponent.innerSectionHeightRatio;
+			var sectionYOffset     = (sectionHeight - innerSectionHeight) / 2.0f;
+			var innerBounds        = new Rect(-usableMapWidth / 2.0f, sectionY + sectionYOffset, usableMapWidth, innerSectionHeight);
+			
+			// TEMP: Need to figure out how to grab platform pieces.
+			// TODO: Pick a random spot inside the usable bounds, but make sure that platforms don't
+			// overlap.
+			var platform                     = GameObject.Instantiate(this.platformPrototypes[0]) as GameObject;
+			var platformComponent            = platform.GetComponent<GGPlatformComponent>();
+			var platformSize                 = platformComponent.size;
+			var usableBounds                 = innerBounds;
+			usableBounds.x                  += platformSize.x / 2.0f;
+			usableBounds.y                  += platformSize.y / 2.0f;
+			usableBounds.width              -= platformSize.x;
+			usableBounds.height             -= platformSize.y;
+			platform.name                    = "Platform";
+			platform.transform.localPosition = new Vector3(usableBounds.x, usableBounds.yMax, 0.0f);
+			
+			// TODO: Random chance to flip the platform horizontally.
+			
+			platform.SetActive(true);
+			this.platformComponents.Add(platformComponent);
+			
+			if (i < divisionCount) {
+				sectionY = divisionPoints[i];
+			}
+		}
+		
 		// The ground should only be active on the very first level. If we're generating any level
 		// other than the first one, then we've made it past the first level.
 		this.groundComponent.gameObject.SetActive(mapIndex == 0);
@@ -75,13 +122,13 @@ public class GGMapComponent: MonoBehaviour {
 	// Adds a wall segment at the given Y position and returns the height of the segment that was
 	// added.
 	private float AddWall(float y, bool isOnLeftSide) {
-		// TEMP: Need to figure out how to grab wall pieces.
 		var x = GGMapComponent.mapWidth / 2.0f;
 		
 		if (isOnLeftSide) {
 			x = -x;
 		}
 		
+		// TEMP: Need to figure out how to grab wall pieces.
 		var wall                     = GameObject.Instantiate(this.wallPrototypes[0]) as GameObject;
 		var wallComponent            = wall.GetComponent<GGWallComponent>();
 		var wallHeight               = wallComponent.height;
@@ -116,7 +163,24 @@ public class GGMapComponent: MonoBehaviour {
 	
 	/* Getting configuration values. */
 	
-	public const float mapWidth     = 28.0f;
-	public const float mapHeight    = 60.0f;
-	public const float groundHeight = 3.0f;
+	// The width of each map. This defines where walls and platforms are placed.
+	public const float mapWidth = 28.0f;
+	
+	// The full height of each map, from the bottom of the screen to the top.
+	public const float mapHeight = 48.0f;
+	
+	// The amount of padding on the sides of the map, which is used for positioning platforms so
+	// that they don't overlap the walls.
+	public const float horizontalMapPadding = 3.5f;
+	
+	// The ratio of the height of the inner bounds of a platform section to the full height of the
+	// section. If this value is 1.0, the inner section will be just as tall as the outer section,
+	// which means platforms from each section will be able to touch each other. Reducing the ratio
+	// ensures that there will be more space between the platforms of each section, but reduces the
+	// variety in vertical positioning of platforms within a section.
+	public const float innerSectionHeightRatio = 0.65f;
+	
+	// The minimum and maximum number of platform sections that can be used for an individual level.
+	public const int minSectionCount = 3;
+	public const int maxSectionCount = 6;
 }
--- Assets/Scripts/GGPlatformComponent.cs
+++ Assets/Scripts/GGPlatformComponent.cs
@@ -11,28 +11,63 @@ public class GGPlatformComponent: MonoBehaviour {
 	
 	public void Start() {
 		var physicsComponent = GGGameSceneComponent.instance.physicsComponent;
-		var transform        = this.transform;
-		var childCount       = transform.childCount;
 		
-		for (var i = 0; i < childCount; i += 1) {
-			var child      = transform.GetChild(i);
-			var collider2D = child.collider2D;
+		foreach (var collider in this.colliders) {
+			var name = collider.gameObject.name;
 			
-			switch (child.name) {
-				case "Grass": collider2D.sharedMaterial = physicsComponent.grassMaterial;     break;
-				case "Dirt":  collider2D.sharedMaterial = physicsComponent.dirtMaterial;      break;
-				case "Sand":  collider2D.sharedMaterial = physicsComponent.sandMaterial;      break;
-				case "Rock":  collider2D.sharedMaterial = physicsComponent.rockMaterial;      break;
-				default:      Debug.LogError("Unhandled platform child name: " + child.name); break;
+			switch (name) {
+				case "Grass": collider.sharedMaterial = physicsComponent.grassMaterial; break;
+				case "Dirt":  collider.sharedMaterial = physicsComponent.dirtMaterial;  break;
+				case "Sand":  collider.sharedMaterial = physicsComponent.sandMaterial;  break;
+				case "Rock":  collider.sharedMaterial = physicsComponent.rockMaterial;  break;
+				default:      Debug.LogError("Unhandled platform child name: " + name); break;
 			}
-			
-			this.colliders.Add(collider2D);
 		}
 	}
 	
 	/* Getting child objects and components. */
 	
 	// The colliders of each portion of the platform.
-	[HideInInspector]
-	public List<Collider2D> colliders = new List<Collider2D>();
+	public List<PolygonCollider2D> colliders { get {
+		if (_colliders == null) {
+			_colliders     = new List<PolygonCollider2D>();
+			var transform  = this.transform;
+			var childCount = transform.childCount;
+			
+			for (var i = 0; i < childCount; i += 1) {
+				_colliders.Add(transform.GetChild(i).GetComponent<PolygonCollider2D>());
+			}
+		}
+		
+		return _colliders;
+	} }
+	
+	private List<PolygonCollider2D> _colliders;
+	
+	/* Getting information about the platform. */
+	
+	// The size of the rectangle that encloses the platform.
+	public Vector2 size { get {
+		if (_size == Vector2.zero) {
+			var minX = float.MaxValue;
+			var maxX = float.MinValue;
+			var minY = float.MaxValue;
+			var maxY = float.MinValue;
+			
+			foreach (var collider in this.colliders) {
+				foreach (var point in collider.points) {
+					minX = Mathf.Min(minX, point.x);
+					maxX = Mathf.Max(maxX, point.x);
+					minY = Mathf.Min(minY, point.y);
+					maxY = Mathf.Max(maxY, point.y);
+				}
+			}
+			
+			_size = new Vector2(maxX - minX, maxY - minY);
+		}
+		
+		return _size;
+	} }
+	
+	private Vector2 _size;
 }
