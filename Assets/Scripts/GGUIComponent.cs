7f05e66969552fed5e1aa4b598dffa32b19fe0a1
Jarod Long <jlong22@me.com>
2015-05-12 22:52:41 -0700

    Hole label is now populated with actual data.

--- Assets/Scripts/GGUIComponent.cs
+++ Assets/Scripts/GGUIComponent.cs
@@ -13,12 +13,24 @@ public class GGUIComponent: MonoBehaviour {
 	public Text sheepCountLabel;
 	public Text sheepCountLabelShadow;
 	
+	public Text holeLabel;
+	public Text holeLabelShadow;
+	
+	public Text strokeCountLabel;
+	public Text strokeCountLabelShadow;
+	
 	/* Updating. */
 	
 	public void FixedUpdate() {
 		var gameSceneComponent     = GGGameSceneComponent.instance;
 		var sheepCountString       = "" + gameSceneComponent.sheepCount;
+		var holeString             = "Hole " + (gameSceneComponent.mapComponent.currentMapIndex + 1);
+		var strokeCountString      = "0"; // TODO
 		sheepCountLabel.text       = sheepCountString;
 		sheepCountLabelShadow.text = sheepCountString;
+		holeLabel.text             = holeString;
+		holeLabelShadow.text       = holeString;
+		strokeCountLabel.text      = strokeCountString;
+		strokeCountLabel.text      = strokeCountString;
 	}
 }
